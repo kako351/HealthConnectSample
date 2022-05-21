14ba80d0430b803d834ffbc492e05494ec664c9d
kako351 <h3.birth@gmail.com>
2022-05-21 09:44:18 +0900

    request permisson

--- app/src/main/java/com/kako351/healthconnectsample/MainActivity.kt
+++ app/src/main/java/com/kako351/healthconnectsample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kako351.healthconnectsample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,22 +9,72 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.permission.HealthDataRequestPermissions
+import androidx.health.connect.client.permission.Permission
+import androidx.health.connect.client.records.HeartRateSeries
+import androidx.health.connect.client.records.Steps
+import androidx.lifecycle.lifecycleScope
 import com.kako351.healthconnectsample.ui.theme.HealthConnectSampleTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    val PERMISSIONS =
+        setOf(
+            Permission.createReadPermission(HeartRateSeries::class),
+            Permission.createWritePermission(HeartRateSeries::class),
+            Permission.createReadPermission(Steps::class),
+            Permission.createWritePermission(Steps::class)
+        )
+
+    val requestPermissions =
+        registerForActivityResult(HealthDataRequestPermissions()) { granted ->
+            if (granted.containsAll(PERMISSIONS)) {
+                // Permissions successfully granted
+            } else {
+                // Lack of required permissions
+            }
+        }
+
+    var healthConnectClient: HealthConnectClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (HealthConnectClient.isAvailable(this)) {
+            // Health Connect is available and installed.
+            healthConnectClient = HealthConnectClient.getOrCreate(this)
+        } else {
+            // ...
+            Log.e("HealthConnectClient", "Failed getOrCreate. HealthConnectClient not available.")
+        }
         setContent {
             HealthConnectSampleTheme {
                 // A surface container using the 'background' color from the theme
+                LaunchedEffect("permission") {
+                    healthConnectClient?.let {
+                        checkPermissionsAndRun(it)
+                    }
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
                 }
+            }
+        }
+    }
+
+    fun checkPermissionsAndRun(client: HealthConnectClient) {
+        lifecycleScope.launch {
+            val granted = client.permissionController.getGrantedPermissions(PERMISSIONS)
+            if (granted.containsAll(PERMISSIONS)) {
+                // Permissions already granted
+            } else {
+                requestPermissions.launch(PERMISSIONS)
             }
         }
     }
